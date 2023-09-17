417b2239a6a0d770203d8c97ef9659daa5090ea0
Raiyan Rizwan <raiyanrizwan@berkeley.edu>
2023-09-17 04:34:22 -0400

    everything is integrated. the bugs have been fixed. the red bull has been doused. hand tracking is a go. queues were used to display images from async to sync. I have escaped the matrix, and entered MIT

--- MediaPipeAdvanced.py
+++ MediaPipeAdvanced.py
@@ -1,8 +1,12 @@
 import mediapipe as mp
+import matplotlib.pyplot as plt
 import numpy as np
-import math
+from queue import Queue
 import cv2
+import socket
 
+# -- QUEUE -- 
+image_queue = Queue()
 
 # -- WEBCAM -- 
 CAM_INDEX = 1
@@ -30,7 +34,28 @@ HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 
 
+# -- UNITY SERVER SOCKET --
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5052) # 5052 random port that's unused
+
+
 # -- UTILITY FUNCTIONS -- 
+def draw_points(img: mp.Image, pose_landmarks, indices=[]):
+    image = np.copy(img.numpy_view())
+    if not indices:
+        indices = list(range(len(pose_landmarks)))
+    for j in indices:
+        x, y = pose_landmarks[j].x, pose_landmarks[j].y
+        x, y = round(x * img.width), round(y * img.height)
+        image = cv2.circle(image, (x, y), radius=5, color=(0, 0, 255), thickness=-1)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image_queue.put(image)
+def geo_center(landmarks):
+    xs, ys = [], []
+    for lm in landmarks:
+        xs.append(lm.x)
+        ys.append(lm.y)
+    return [sum(xs) / len(xs), sum(ys) / len(ys)]    
 def transform_hand_to_pose(landmarks, a, b):
     pose_world_hand_landmarks = []
     for lm in landmarks:
@@ -40,10 +65,6 @@ def transform_hand_to_pose(landmarks, a, b):
         lm_new.append(a[2]*lm.z+b[2])
         pose_world_hand_landmarks.append(lm_new)
     return pose_world_hand_landmarks
-    xs = [lm.x for lm in landmarks]
-    ys = [lm.y for lm in landmarks]
-    zs = [lm.z for lm in landmarks]
-    return ([sum(xs) / len(xs), sum(ys) / len(ys), sum(zs) / len(zs)])
 def find_ab(z1, z2, z1_prime, z2_prime):
     # format z1: [x, y, z]
     a_values = []
@@ -67,37 +88,54 @@ def crop_image(image: mp.Image, min_x, max_x, min_y, max_y):
     return cropped_image
 def find_hand_sub_image(hand: list, image: mp.Image):
     """Return the image of the hand within the picture."""
+    
+    if any([hand[i].visibility < 0.2 for i in range(len(hand))]):
+        return None
+    
     w, h = image.width, image.height
-    wrist, thumb, index = hand
-    hand_cord_length = math.sqrt((wrist.x - index.x)**2 + (thumb.y - index.y)**2)
-    buffer = hand_cord_length
+    xc, yc = geo_center(hand)
     
-    min_x = round(max(min(wrist.x, thumb.x, index.x) - buffer, 0) * w)
-    max_x = round(min(max(wrist.x, thumb.x, index.x) + buffer, 1) * w)
+    xcp, ycp = xc * w, yc * h
+    buffer = max(w, h) // 4 # pixels
     
-    min_y = round(max(min(wrist.y, thumb.y, index.y) - buffer, 0) * h)
-    max_y = round(min(max(wrist.y, thumb.y, index.y) + buffer, 1) * h)
+    min_x = int(max(xcp - buffer, 0))
+    max_x = int(min(xcp + buffer, w))
     
-    if (max_y < min_y or max_x < min_x):
+    min_y = int(max(ycp - buffer, 0))
+    max_y = int(min(ycp + buffer, h))
+    
+    w_crop = max_x - min_x
+    h_crop = max_y - min_y
+    
+    if w_crop <= 0 or h_crop <= 0:
         return None
     
     sub_image = crop_image(image, min_x, max_x, min_y, max_y)
-    
+
     return sub_image
-def unity_format_cords(pose: list):
+def unity_landmarks(landmarks: list):
     """Create output of the form x1, y1, z1, x2, y2, z2, x3, y3,"""
     joints = [0, 20, 22, 16, 14, 12, 24, 26, 28, 32, 30, 29, 27, 31, 25, 23, 11, 13, 15, 21, 19]
     output = ""
     for j in joints:
-        output += f'{pose[j].x},{pose[j].y},{pose[j].z},'
+        output += f'{landmarks[j].x},{landmarks[j].y},{landmarks[j].z},'
     output = output[:-1]
     return output
+def unity_array(coords: list):
+    output = ""
+    for coord in coords:
+        output += f'{coord[0]}, {coord[1]}, {coord[2]},'
+    return output[:-1]
 def pose_call_back(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     """This function gets called when the pose model identifies a pose within a video frame. The hand model is triggered here."""
     if result.pose_world_landmarks and result.pose_landmarks:
         pose_world = result.pose_world_landmarks[0]
         pose_landmarks = result.pose_landmarks[0]
-        pose_unity_format = unity_format_cords(pose_world)
+        pose_unity_format = unity_landmarks(pose_world)
+        
+        # draw the pose
+        joints = [0, 20, 22, 16, 14, 12, 24, 26, 28, 32, 30, 29, 27, 31, 25, 23, 11, 13, 15, 21, 19]
+        draw_points(output_image, pose_landmarks, joints)
         
         # wrist, thumb, index
         left_hand = [pose_landmarks[15], pose_landmarks[21], pose_landmarks[19]] 
@@ -108,52 +146,67 @@ def pose_call_back(result: PoseLandmarkerResult, output_image: mp.Image, timesta
 
         with HandLandmarker.create_from_options(hand_options) as landmarker:
             if left_hand_image:
+                
                 # detection algorithm on left hand
                 hand_landmarker_left_result = landmarker.detect(left_hand_image)
                 
-                if hand_landmarker_left_result.hand_world_landmarks:
+                # view mp image of hand
+                mp_np_image = left_hand_image.numpy_view()
+                mp_np_image = cv2.cvtColor(mp_np_image, cv2.COLOR_RGB2BGR)
+                
+                # view hand landmarks
+                if hand_landmarker_left_result.hand_landmarks:
+                    draw_points(mp.Image(image_format=output_image.image_format, data=mp_np_image), hand_landmarker_left_result.hand_landmarks[0])
+                
+                    hand_world_landmarks = hand_landmarker_left_result.hand_world_landmarks[0]
+                    
                     # wrist and thumb in pose world coordinates
                     wrist_left = [pose_world[15].x, pose_world[15].y, pose_world[15].z]
                     thumb_left = [pose_world[21].x, pose_world[21].y, pose_world[21].z]
                     
                     # wrist and thumb in hand world coordinates
-                    wrist_left_prime = hand_landmarker_left_result.hand_world_landmarks[0]
+                    wrist_left_prime = hand_world_landmarks[0]
                     wrist_left_prime = [wrist_left_prime.x, wrist_left_prime.y, wrist_left_prime.z]
-                    thumb_left_prime = hand_landmarker_left_result.hand_world_landmarks[4]
+                    thumb_left_prime = hand_world_landmarks[4]
                     thumb_left_prime = [thumb_left_prime.x, thumb_left_prime.y, thumb_left_prime.z]
                     
                     # transform parameters
                     a, b = find_ab(wrist_left, thumb_left, wrist_left_prime, thumb_left_prime)
-                    pose_world_hand_coordinates = transform_hand_to_pose(hand_landmarker_left_result.pose_world)
+                    pose_world_hand_coordinates = transform_hand_to_pose(hand_world_landmarks, a, b)
                     
                     # final output
-                    left_hand_unity_format = unity_format_cords(pose_world_hand_coordinates)
-                    pose_unity_format.append(left_hand_unity_format)
+                    left_hand_unity_format = unity_array(pose_world_hand_coordinates)
+                    pose_unity_format += left_hand_unity_format
                 
             if right_hand_image:
                 hand_landmarker_right_result = landmarker.detect(right_hand_image)
-
-                if hand_landmarker_right_result.hand_world_landmarks:
+                
+                if hand_landmarker_right_result.hand_landmarks:
+                    draw_points(mp.Image(image_format=output_image.image_format, data=mp_np_image), hand_landmarker_right_result.hand_landmarks[0])
+                
+                    hand_world_landmarks = hand_landmarker_right_result.hand_world_landmarks[0]
+                    
                     # wrist and thumb in pose world coordinates
                     wrist_right = [pose_world[16].x, pose_world[16].y, pose_world[16].z]
                     thumb_right = [pose_world[22].x, pose_world[22].y, pose_world[22].z]
                     
                     # wrist and thumb in hand world coordinates
-                    wrist_right_prime = hand_landmarker_right_result.hand_world_landmarks[0]
+                    wrist_right_prime = hand_world_landmarks[0]
                     wrist_right_prime = [wrist_right_prime.x, wrist_right_prime.y, wrist_right_prime.z]
-                    thumb_right_prime = hand_landmarker_right_result.hand_world_landmarks[4]
+                    thumb_right_prime = hand_world_landmarks[4]
                     thumb_right_prime = [thumb_right_prime.x, thumb_right_prime.y, thumb_right_prime.z]
                     
                     # transform parameters
                     a, b = find_ab(wrist_right, thumb_right, wrist_right_prime, thumb_right_prime)
-                    pose_world_hand_coordinates = transform_hand_to_pose(hand_landmarker_right_result.pose_world)
+                    pose_world_hand_coordinates = transform_hand_to_pose(hand_world_landmarks, a, b)
                     
                     # final output
-                    right_hand_unity_format = unity_format_cords(pose_world_hand_coordinates)
-                    pose_unity_format.append(right_hand_unity_format)
+                    right_hand_unity_format = unity_array(pose_world_hand_coordinates)
+                    pose_unity_format += right_hand_unity_format
 
-        print(pose_unity_format)
-        return pose_unity_format
+        # send to unity via socket
+        sock.sendto(str.encode(pose_unity_format), serverAddressPort)
+
 
 # -- MODEL OPTIONS -- 
 pose_options = PoseLandmarkerOptions(
@@ -188,13 +241,10 @@ with PoseLandmarker.create_from_options(pose_options) as landmarker:
         
         # timestep
         frame_timestamp_ms += timestep
-     
         
-# -- NOT USED --
-def draw_points(frame, pose_landmarks):
-    image = np.copy(frame.numpy_view())
-    joints = [0, 20, 22, 16, 14, 12, 24, 26, 28, 32, 30, 29, 27, 31, 25, 23, 11, 13, 15, 21, 19]
-    for j in joints:
-        x, y = pose_landmarks[j].x, pose_landmarks[j].y
-        x, y = round(x * frame.width), round(y * frame.height)
-        image = cv2.circle(image, (x, y), radius=5, color=(0, 0, 255), thickness=-1)
+        # check image queue
+        if not image_queue.empty():
+            mp_np_image = image_queue.get()
+            plt.imshow(mp_np_image)
+            plt.axis('off')
+            plt.show()
